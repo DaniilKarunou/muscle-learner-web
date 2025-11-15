633c3dbf290554288a5f8e81226ceec25df1716f
BashEbash <pidppid@gmail.com>
2025-11-15 14:30:17 +0100

    Setup GH Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MuscleSystemScreen from "./ui/MuscleSystemScreen";
 import MuscleRegionScreen from "./ui/MuscleRegionScreen";
 import MuscleSubGroupScreen from "./ui/MuscleSubGroupScreen";
@@ -10,18 +10,16 @@ import Layout from "./ui/Layout";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Layout>
-                <Routes>
-                    <Route path="/" element={<MuscleSystemScreen />} />
-                    <Route path="/region/:regionId" element={<MuscleRegionScreen />} />
-                    <Route path="/subgroup/:subGroupId" element={<MuscleSubGroupScreen />} />
-                    <Route path="/detail/:muscleId" element={<MuscleDetail />} />
-                    <Route path="/quiz/:scope/:id" element={<QuizScreen />} />
-                    <Route path="/quizResult/:score/:total" element={<QuizResultScreen />} />
-                </Routes>
-            </Layout>
-        </Router>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<MuscleSystemScreen />} />
+                <Route path="/region/:regionId" element={<MuscleRegionScreen />} />
+                <Route path="/subgroup/:subGroupId" element={<MuscleSubGroupScreen />} />
+                <Route path="/detail/:muscleId" element={<MuscleDetail />} />
+                <Route path="/quiz/:scope/:id" element={<QuizScreen />} />
+                <Route path="/quizResult/:score/:total" element={<QuizResultScreen />} />
+            </Routes>
+        </Layout>
     );
 };
 
